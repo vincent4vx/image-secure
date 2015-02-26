15f5628ab56bd68cda7356cf4ee023a813da18fb
Thomas Munoz <thomas.munoz30@gmail.com>
2015-02-26 23:27:02 +0100

    Fix upload + canvas

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,76 @@
  */
 var fileHandler = {};
 
-fileHandler.uploadFile = function(file){
-    console.log('UPLOAD FILE');
-    $('.content').append('<div class="progress"></div>');
-    $('.progress').append('<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0"' + ' aria-valuemax="100" style="min-width: 2em;">0%</div>');
+fileHandler.launch = function(file){
+    /*$('.content').append('<div class="progress"></div>');
+    $('.progress').append('<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0"' + ' aria-valuemax="100" style="min-width: 2em;">0%</div>');*/
     file = file[0];
-    console.log(file);
+
+    var contentSave = $('.jumbotron').html();
+
+    $('.jumbotron').empty();
+    $('.jumbotron').append(
+        $('<h3 />')
+            .text('Image selectionnée : ' + file.name),
+        $('<img>')
+            .attr('id', 'hiddenImage')
+    );
+
+    var fileReader = new FileReader();
+    fileReader.onload = function (e){
+
+        $('#hiddenImage').attr('src', e.target.result)
+        var width = $('#hiddenImage').width();
+        var height = $('#hiddenImage').height();
+
+        $('.jumbotron').append(
+            $('<canvas />')
+                .addClass('img-responsive')
+                .addClass('center-block')
+                .attr('width', width)
+                .attr('height', height)
+                .attr('id', 'canvas-uploaded')
+        );
+
+        var canvas = $('#canvas-uploaded')[0];
+        var context = canvas.getContext('2d');
+
+        $('#hiddenImage').load(function() {
+           context.drawImage(this, 0, 0);
+           $('#hiddenImage').remove();
+           fileHandler.generateForm();
+        });
+    };
+    fileReader.readAsDataURL(file);
+};
+
+fileHandler.generateForm = function(){
+    $('.jumbotron').append(
+            $('<p />')
+                .text('Si vous le souhaitez, vous pouvez saisir votre clé privée afin de crypter votre image' +
+                ', sinon, une clé sera générée automatiquement par l\'application'),
+            $('<form />')
+                .attr('method', 'POST')
+                .attr('action', '#')
+                .addClass('form-horizontal')
+                .append(
+                $('<textarea />')
+                    .addClass('form-control')
+                    .attr('rows', '3'),
+                $('<button />')
+                    .attr('type', 'submit')
+                    .addClass('btn')
+                    .addClass('btn-primary')
+                    .text('Envoyer')
+            )
+        );
+};
+
+fileHandler.encrypt = function(){
+
+};
+
+fileHandler.upload = function(file){
     var formData = new FormData();
     formData.append('file', file);
 
@@ -22,10 +86,11 @@ fileHandler.uploadFile = function(file){
             var xhr = new window.XMLHttpRequest();
             xhr.upload.addEventListener("progress", function(e) {
                 if (e.lengthComputable) {
-                    var percentComplete = e.loaded / e.total;
-                    percentComplete = (percentComplete * 100).toFixed(2);
-                    console.log(percentComplete);
-                    if (percentComplete === 100) {
+                    var percentCompleted = e.loaded / e.total;
+                    percentCompleted = (percentComplete * 100).toFixed(2);
+                    console.log(percentCompleted);
+                    if (percentCompleted === 100) {
+                        console.log('finish');
                     }
                 }
             }, false);
@@ -61,7 +126,7 @@ $(document).ready(function(){
             $('.jumbotron .content').css('opacity', '1');
             var file = e.originalEvent.dataTransfer.files;
             $('#drag_message').remove();
-            fileHandler.uploadFile(file);
+            fileHandler.launch(file);
         }
     });
 });
