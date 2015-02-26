d1e151a6121baf662331e86e9bf0ad66153f8619
Thomas Munoz <thomas.munoz30@gmail.com>
2015-02-26 18:42:37 +0100

    Add (working) upload

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,35 @@ var fileHandler = {};
 fileHandler.uploadFile = function(file){
     console.log('UPLOAD FILE');
     $('.content').append('<div class="progress"></div>');
-    $('.progress').append('<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0"' +
-                            ' aria-valuemax="100" style="min-width: 2em;">0%</div>');
+    $('.progress').append('<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0"' + ' aria-valuemax="100" style="min-width: 2em;">0%</div>');
+    file = file[0];
+    console.log(file);
+    var formData = new FormData();
+    formData.append('file', file);
+
+    $.ajax({
+        url: '/upload',
+        type: 'POST',
+        processData: false,
+        contentType: false,
+        data: formData,
+        xhr: function() {
+            var xhr = new window.XMLHttpRequest();
+            xhr.upload.addEventListener("progress", function(e) {
+                if (e.lengthComputable) {
+                    var percentComplete = e.loaded / e.total;
+                    percentComplete = (percentComplete * 100).toFixed(2);
+                    console.log(percentComplete);
+                    if (percentComplete === 100) {
+                    }
+                }
+            }, false);
+            return xhr;
+        },
+        success: function(data){
+            console.log(data);
+        }
+    });
 };
 
 $(document).ready(function(){
